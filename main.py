46e1a6b9c06ba2e33d07e0599a2bf56861899ecb
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-14 07:21:41 +0000

    update 1

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
 
+pdf = FPDF()
 my_pdf = FPDF(orientation="p", format="A4")
 
 # adds page to document
-my_pdf.add_page()
+my_pdf = pdf.add_page()
+
+image1 = pdf.image("shadow-of-war.png", x=85, y=10, w=40, h=40, link="https://www.shadowofwar.com/purchase/")
 
 # name document and add its path
-my_pdf.output("project.pdf")
+my_pdf = pdf.output("project.pdf")
+
+
