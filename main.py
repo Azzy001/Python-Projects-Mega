fbba0a3ac985604c9858d0484f70aa91aafe62d1
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-15 06:29:26 +0000

    update 3

--- main.py
+++ main.py
@@ -8,22 +8,34 @@ pdf = FPDF(orientation="p", format="A4")
 # adds page to document
 pdf.add_page()
 
+"""
 # adding images to document (with hyperlink)
-pdf.image("shadow-of-war.png", x=175, y=5, w=30, h=30, link="https://www.shadowofwar.com/purchase/")
-pdf.image("shadow-of-war2.png", x=175, y=35, w=30, h=30, link="")
-pdf.image("shadow-of-war3.png", x=175, y=65, w=30, h=30, link="")
+pdf.image("", x=175, y=5, w=30, h=30, link="")
+pdf.image("", x=175, y=37, w=30, h=30, link="")
+pdf.image("", x=175, y=70, w=30, h=30, link="")
+"""
 
 pdf.set_font("Arial", size=17)
-pdf.cell(5, 10, txt="Why you should play Shadow of war in 2022!")
+pdf.cell(w=0, h=5, txt="Why should you learn Python in 2022?", ln=1)
 
 pdf.set_font("Arial", size=10)
-pdf.text(5, 18, txt="By Arsalan Arref")
+pdf.cell(w=0, h=10, txt="By Arsalan Arref", ln=1)
 
+# set position
+pdf.set_xy(x=10, y=30)
+pdf.set_font("Arial", style="B", size=10)
+pdf.cell(w=0, h=5, txt="Temp", ln=1)
 
+pdf.set_font("Arial", size=10)
+pdf.multi_cell(w=0, h=5, txt="Python is the easiest programming language to learn,"+
+                             " it is simple, easy to read, and it features english syntax. Therefore,"+
+                             " If you are new to programming, then Python is your goto!")
 
-pdf.cell(w=0, h=5, txt="Test", border=1, align="C", ln=1)
+pdf.set_xy(x=10, y=55)
+pdf.set_font("Arial", style="B", size=10)
+pdf.cell(w=0, h=5, txt="What can you do with Python?")
 
-pdf.multi_cell(w=0, h=5, txt="In this article, I will be sharing my opinion on why I think that this game still rocks, \neven though its over 5 years old.", border=1)
+pdf.cell(w=0, h=5, txt="Temp", border=1, ln=1)
 
 # name document and add its path
 pdf.output("project.pdf")
