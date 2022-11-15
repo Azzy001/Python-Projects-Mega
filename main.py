581d35fcb5fc3195dae5b8a08f7b7e090b473429
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-15 07:32:59 +0000

    update 3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pdf.add_page()
 # adding images to document (with hyperlink)
 pdf.image("", x=175, y=5, w=30, h=30, link="")
 pdf.image("", x=175, y=37, w=30, h=30, link="")
-pdf.image("", x=175, y=70, w=30, h=30, link="")
+
 """
 
 pdf.set_font("Arial", size=17)
@@ -33,9 +33,49 @@ pdf.multi_cell(w=0, h=5, txt="Python is the easiest programming language to lear
 
 pdf.set_xy(x=10, y=55)
 pdf.set_font("Arial", style="B", size=10)
-pdf.cell(w=0, h=5, txt="What can you do with Python?")
+pdf.cell(w=0, h=5, txt="What can you do with Python?", ln=1)
 
-pdf.cell(w=0, h=5, txt="Temp", border=1, ln=1)
+pdf.set_font("Arial", size=10)
+pdf.multi_cell(w=0, h=5, txt="The things you can do with Python are endless! you can create softwares, "+
+                        " websites, automation, data visualization, games, and so much more. "+
+                        " In this article, I will give examples of 5 real-world applications, used by Python.")
+
+# 1. healthcare
+pdf.set_xy(x=10, y=75)
+pdf.set_font("Arial", style="B", size=10)
+pdf.cell(w=0, h=5, txt="        1. Healthcare", ln=1)
+
+pdf.set_font("Arial", size=10)
+pdf.multi_cell(w=0, h=5, txt="The medical industries are able to make better decisions with accurate predictions "+
+                             "by using Python-powered applications, "+
+                             "for example, using Python for image diagnostics or detecting and classifying tumors. "+
+                             "Check out this link for more information: "+
+                             "https://www.datacamp.com/blog/python-in-healthcare-ai-applications-in-hospitals")
+
+# healthcare image
+pdf.image("healthcare.jpg", x=10, y=100, w=75, h=45)
+
+pdf.set_xy(x=90, y=100)
+pdf.multi_cell(w=0, h=5, txt="Examples of Python applications used in Healthcare:\n" +
+                                       "* Parkinson's disease detection\n"+
+                                       "* Heart disease detection\n"+
+                                       "* Diabetes prediction\n"+
+                                       "* Breast cancer detection")
+
+
+pdf.set_xy(x=10, y=150)
+pdf.set_font("Arial", style="B", size=10)
+pdf.cell(w=0, h=5, txt="        2. Data visualization", ln=1)
+
+pdf.set_font("Arial", size=10)
+pdf.multi_cell(w=0, h=5, txt="Data visualization "+
+                                        ""+
+                                        ""+
+                                        ""+
+                                        "")
+
+
+
 
 # name document and add its path
 pdf.output("project.pdf")
